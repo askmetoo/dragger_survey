c19e9d96c24e7feb7a27b4486ab8913b538ba347
dthuesen <dthuesen@gmx.de>
2019-08-23 19:05:08 +0200

    update packages

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/dthuesen/Google Drive/sites/21_Flutter-Apps/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/dthuesen/Documents/01_dev/flutter_projects/dragger_survey"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/dthuesen/Google Drive/sites/21_Flutter-Apps/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
