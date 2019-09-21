82160ed4d9a82abc64876e610a9686257229470b
dthuesen <dthuesen@gmx.de>
2019-09-21 13:54:51 +0200

    Fix issue with rebuilding dragger board

--- lib/src/screens/dragger_screen.dart
+++ lib/src/screens/dragger_screen.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dragger_survey/src/blocs/blocs.dart';
 import 'package:dragger_survey/src/screens/screens.dart';
 import 'package:dragger_survey/src/widgets/widgets.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -49,29 +48,22 @@ class _DraggerScreenState extends State<DraggerScreen> {
       log("In Dragger Screen prismSurveySetBloc.currentPrismSurveySet == null: ${prismSurveySetBloc.currentPrismSurveySet == null}");
     }
 
-    return FutureBuilder<FirebaseUser>(
-        future: signInBloc.currentUser,
-        builder: (BuildContext context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    BuildBoard(xLabel: _xName, yLabel: _yName),
-                    Text(
-                        """Stone is dragged to \n$_xName: ${prismSurveyBloc.rowIndex} \n$_xName:  ${prismSurveyBloc.colIndex}"""),
-                    DraggerBoardButtonRow(),
-                  ],
-                ),
-              ),
-            );
-          }
-          return Container();
-        });
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            BuildBoard(xLabel: _xName, yLabel: _yName),
+            Text(
+                """Stone is dragged to \n$_xName: ${prismSurveyBloc.rowIndex} \n$_xName:  ${prismSurveyBloc.colIndex}"""),
+            DraggerBoardButtonRow(),
+          ],
+        ),
+      ),
+    );
   }
 }
 
