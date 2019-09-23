114f98e487895329f606d6e65962098b1a13dc39
dthuesen <dthuesen@gmx.de>
2019-09-23 17:22:53 +0200

    Sign in with User creation in db works

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -2,15 +2,15 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dragger_survey/src/blocs/blocs.dart';
 import 'package:dragger_survey/src/enums/connectivity_status.dart';
-import 'package:dragger_survey/src/screens/screens.dart';
 import 'package:dragger_survey/src/services/models.dart';
 import 'package:dragger_survey/src/styles.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' as prefix0;
 import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
+  final GlobalKey _globalLoginKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     FirebaseUser _user = Provider.of<FirebaseUser>(context);
@@ -87,23 +87,22 @@ class LoginScreen extends StatelessWidget {
             Container();
           }
           return Center(
-            child: _signInButton(context: context),
+            child: _signInButton(signInButtonContext: context),
           );
         });
   }
 
-  Widget _signInButton({BuildContext context}) {
-    final SignInBloc signInBloc = Provider.of<SignInBloc>(context);
-    final UserBloc userBloc = Provider.of<UserBloc>(context);
-    FirebaseUser _user = Provider.of<FirebaseUser>(context);
+  Widget _signInButton({BuildContext signInButtonContext}) {
+    final SignInBloc signInBloc = Provider.of<SignInBloc>(signInButtonContext);
+    final UserBloc userBloc = Provider.of<UserBloc>(signInButtonContext);
+    FirebaseUser _user = Provider.of<FirebaseUser>(signInButtonContext);
     bool loggedIn = _user != null;
-    GlobalKey _globalLoginKey = GlobalKey();
 
     FirebaseUser _returnedLoginFirebaseUser;
 
     return FutureBuilder<FirebaseUser>(
         future: signInBloc.currentUser,
-        builder: (BuildContext context,
+        builder: (signInButtonContext,
             AsyncSnapshot<FirebaseUser> currentUserSnapshot) {
           if (currentUserSnapshot.connectionState == ConnectionState.none ||
               currentUserSnapshot.connectionState == ConnectionState.waiting ||
@@ -117,6 +116,7 @@ class LoginScreen extends StatelessWidget {
             log("In LoginScreen value of 'currentUserSnapshot': ${currentUserSnapshot?.data?.displayName}");
             log("In LoginScreen value of 'loggedIn': $loggedIn");
             log("In LoginScreen value of '_user': $_user");
+
             return Column(
               children: <Widget>[
                 Padding(
@@ -132,28 +132,41 @@ class LoginScreen extends StatelessWidget {
                   splashColor: Styles.drg_colorSecondary,
                   onPressed: () async {
                     log("In LoginScreen OutlineButton 'Sign-In with Google' pressed");
-                    _returnedLoginFirebaseUser = await signInBloc.signInWithGoogle().catchError(
-                          (error) => debugPrint(
-                              """ERROR in login_screen _signInButton() after
+                    _returnedLoginFirebaseUser =
+                        await signInBloc.signInWithGoogle().catchError(
+                              (error) => debugPrint(
+                                  """ERROR in login_screen _signInButton() after
                                  signInBloc.signInWithGoogle()
                                  - error: $error"""),
-                        );
+                            );
                     log("In LoginScreen OutlineButton value of fbUser.displayName: ${_returnedLoginFirebaseUser.displayName}");
                     log("In LoginScreen OutlineButton value of fbUser.uid: ${_returnedLoginFirebaseUser.uid}");
 
-                    QuerySnapshot returnedUser = await userBloc
+                    QuerySnapshot returnedUserQuerySnapshot = await userBloc
                         .getUsersQuery(
                             fieldName: 'providersUID',
-                            fieldValue: currentUserSnapshot.data?.uid)
+                            fieldValue: _returnedLoginFirebaseUser.uid)
                         .catchError(
                           (error) => log(
                               "ERROR in login_screen with getUsersQuery: $error"),
                         );
 
-                    if (returnedUser == null ||
-                        currentUserSnapshot.data?.uid !=
-                            returnedUser.documents[0]['providersUID']) {
+                    if (returnedUserQuerySnapshot == null ||
+                        returnedUserQuerySnapshot.documents.isEmpty) {
+                      log("In LoginScreen OutlineButton returnedUserQuerySnapshot is empty}");
+                    }
+                    if (returnedUserQuerySnapshot.documents.isEmpty ||
+                        _returnedLoginFirebaseUser?.uid !=
+                            returnedUserQuerySnapshot
+                                ?.documents?.first?.data['providersUID']) {
+                      // if (_returnedLoginFirebaseUser?.uid !=
+                      //         returnedUserQuerySnapshot.documents.first.data['providersUID']) {
                       log("In LoginScreen - USER not id DB");
+                      try {
+                        log("returnedUserQuerySnapshot?.documents[0]['providersUID']: ${returnedUserQuerySnapshot?.documents[0]['providersUID']}");
+                      } catch (e) {
+                        log("ERROR returnedUserQuerySnapshot?.documents[0]['providersUID'] is empty: $e");
+                      }
                       log("_returnedLoginFirebaseUser?.uid: ${_returnedLoginFirebaseUser?.uid}");
                       log("_returnedLoginFirebaseUser?.displayName: ${_returnedLoginFirebaseUser?.displayName}");
                       try {
@@ -170,35 +183,45 @@ class LoginScreen extends StatelessWidget {
                         print(
                             "------------------------------------------------------");
                         print("Added data:");
-                        var returnedAddedataFromDb = await userBloc
+                        QuerySnapshot returnedAddedataFromDb = await userBloc
                             .getUsersQuery(
                                 fieldName: 'providersUID',
                                 fieldValue: _returnedLoginFirebaseUser?.uid)
                             .catchError((error) => log(
                                 "ERROR In login_screen getUsersQuery: $error"));
                         print("RETURNED USER DATA after adding to DB:");
-                        print("${returnedAddedataFromDb.documents[0]['providersUID']}");
-                        print("${returnedAddedataFromDb.documents[0]['displayName']}");
-                        print("${returnedAddedataFromDb.documents[0]['email']}");
-                        print("${returnedAddedataFromDb.documents[0]['photoUrl']}");
-                        print("${returnedAddedataFromDb.documents[0]['providerId']}");
+                        print(
+                            "${returnedAddedataFromDb.documents[0]['providersUID']}");
+                        print(
+                            "${returnedAddedataFromDb.documents[0]['displayName']}");
+                        print(
+                            "${returnedAddedataFromDb.documents[0]['email']}");
+                        print(
+                            "${returnedAddedataFromDb.documents[0]['photoUrl']}");
+                        print(
+                            "${returnedAddedataFromDb.documents[0]['providerId']}");
                         print(
                             "ROUTING NEW USER to first screen '/surveysetslist");
-                        Navigator.pushNamed(_globalLoginKey.currentContext,
-                                '/surveysetslist')
-                            .catchError((err) => print(
-                                "ERROR In 'login_screen' routing to pushedNamed: $err"));
                       } catch (err) {
                         print(
                             "ERROR in 'login_screen' with adding User to DB: $err");
                       }
+                      Navigator.pushNamed(
+                              signInButtonContext, '/surveysetslist')
+                          // Navigator.pushNamed(_globalLoginKey.currentContext,
+                          //         '/surveysetslist')
+                          .catchError((err) => print(
+                              "ERROR In 'login_screen' routing to pushedNamed: $err"));
                     } else if (loggedIn ||
-                        returnedUser.documents.first.data.isNotEmpty ||
+                        returnedUserQuerySnapshot
+                            .documents.first.data.isNotEmpty ||
                         currentUserSnapshot.data?.uid ==
-                            returnedUser.documents[0]['providersUID']) {
+                            returnedUserQuerySnapshot.documents[0]
+                                ['providersUID']) {
                       log("USER found id DB");
                       print("ROUTING to first screen '/surveysetslist");
-                      Navigator.pushNamed(_globalLoginKey?.currentContext, 'surveysetslist');
+                      await Navigator.pushNamed(
+                          signInButtonContext, '/surveysetslist');
                     } else {
                       print("In 'login_screen' it's xmas time!!!");
                     }
@@ -277,7 +300,7 @@ class LoginScreen extends StatelessWidget {
         });
   }
 
-  Text _getConnectionStatusText({BuildContext context}) {
+  Widget _getConnectionStatusText({BuildContext context}) {
     final connectionStatus = Provider.of<ConnectivityStatus>(context);
 
     print("connectionStatus: $connectionStatus");
@@ -287,10 +310,22 @@ class LoginScreen extends StatelessWidget {
         connectionStatus == ConnectivityStatus.Offline) {
       return Text(
         "You are connected via ${connectionStatus.toString().split('.')[1]}",
-        style:
-            TextStyle(fontSize: 20, color: Styles.drg_colorSecondaryDeepDark),
+        style: TextStyle(
+          fontSize: 14,
+          color: Styles.drg_colorSecondaryDeepDark.withOpacity(0.6),
+        ),
       );
     }
-    return Text("Don't know more about the connection");
+    return Container();
+    // return Padding(
+    //   padding: const EdgeInsets.only(top: 14.0),
+    //   child: Text(
+    //     "No connection status available",
+    //     style: TextStyle(
+    //       fontSize: 14,
+    //       color: Styles.drg_colorText.withOpacity(0.6),
+    //     ),
+    //   ),
+    // );
   }
 }
