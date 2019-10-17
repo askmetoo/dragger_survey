14210b1702247cb08ef607f6748055d7a866bbd6
dthuesen <dthuesen@gmx.de>
2019-10-17 18:35:29 +0200

    Revert "flutter updated generated file"
    This reverts commit b32395641a140de2cca2d18cef589da6fa79a615.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/dthuesen/Google Drive/sites/21_Flutter-Apps/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/dthuesen/Documents/01_dev/flutter_projects/dragger_survey"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/dthuesen/Documents/01_dev/flutter_projects/dragger_survey/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/dthuesen/Google Drive/sites/21_Flutter-Apps/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
