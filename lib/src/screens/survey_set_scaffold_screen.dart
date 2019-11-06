42bcc477cc4d90317e1d9ecb3cd4ae898db78f8f
dthuesen <dthuesen@gmx.de>
2019-11-06 08:45:06 +0100

    Show Graph only with more than 1 survey (for better data anonymization)

--- lib/src/screens/survey_set_scaffold_screen.dart
+++ lib/src/screens/survey_set_scaffold_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dragger_survey/src/blocs/blocs.dart';
 import 'package:dragger_survey/src/screens/screens.dart';
 import 'package:dragger_survey/src/styles.dart';
@@ -31,60 +32,75 @@ class _SurveySetScaffoldScreenState extends State<SurveySetScaffoldScreen> {
     final PrismSurveyBloc surveyBloc = Provider.of<PrismSurveyBloc>(context);
     final PrismSurveySetBloc surveySetBloc =
         Provider.of<PrismSurveySetBloc>(context);
-    return Scaffold(
-      backgroundColor: Styles.drg_colorAppBackground,
-      endDrawer: UserDrawer(),
-      appBar: AppBar(
-        actions: <Widget>[
-          SignedInUserCircleAvatar(),
-        ],
-        title: Text("${_titleOptions.elementAt(_selectedIndex)}"),
-      ),
-      body: _widgetOptions(context, args: arg).elementAt(_selectedIndex),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton.extended(
-        backgroundColor: Styles.drg_colorSecondary,
-        label: Text(
-          "New Survey",
-          style: TextStyle(
-            color: Styles.drg_colorText.withOpacity(0.8),
-          ),
-        ),
-        icon: Icon(
-          Icons.group_add,
-          color: Styles.drg_colorDarkerGreen,
-        ),
-        tooltip: "Add new Survey",
-        onPressed: () {
-          surveyBloc.currentAskedPerson = 'Anonymous';
-          surveySetBloc.setCurrentPrismSurveySetId(id: arg['id']);
-          Navigator.pushNamed(context, '/draggerscaffold');
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        key: ValueKey(arg.hashCode),
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            title: Text('Details'),
-            icon: Icon(Icons.dvr),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.show_chart),
-            title: Text('Graphs'),
-          ),
-          // BottomNavigationBarItem(
-          //   icon: Icon(Icons.apps),
-          //   title: Text('Dragger Board'),
-          // ),
-        ],
-        currentIndex: _selectedIndex,
-        backgroundColor: Styles.drg_colorSecondary,
-        unselectedItemColor: Styles.drg_colorAppBackground,
-        selectedItemColor: Styles.drg_colorContrast,
-        onTap: (int index) => setState(() => _selectedIndex = index),
-      ),
-    );
+
+    surveySetBloc.currentPrismSurveySet.then((val) =>
+        print("ooooooo===============> val.data.length: ${val.data.length}"));
+
+    return FutureBuilder<QuerySnapshot>(
+        future: surveyBloc.getPrismSurveyQuery(
+            fieldName: 'surveySet', fieldValue: arg['id']),
+        builder: (context, surveyIDsSnapshot) {
+          if (surveyIDsSnapshot.connectionState != ConnectionState.done) {
+            // TODO:
+            return CircularProgressIndicator();
+          } else if (!surveyIDsSnapshot.hasData) {
+            return Container();
+          }
+
+          bool moreSurveyThanOne = surveyIDsSnapshot.data.documents.length > 1;
+
+          return Scaffold(
+              backgroundColor: Styles.drg_colorAppBackground,
+              endDrawer: UserDrawer(),
+              appBar: AppBar(
+                actions: <Widget>[
+                  SignedInUserCircleAvatar(),
+                ],
+                title: Text("${_titleOptions.elementAt(_selectedIndex)}"),
+              ),
+              body:
+                  _widgetOptions(context, args: arg).elementAt(_selectedIndex),
+              floatingActionButtonLocation:
+                  FloatingActionButtonLocation.centerFloat,
+              floatingActionButton: FloatingActionButton.extended(
+                backgroundColor: Styles.drg_colorSecondary,
+                label: Text(
+                  "New Survey",
+                  style: TextStyle(
+                    color: Styles.drg_colorText.withOpacity(0.8),
+                  ),
+                ),
+                icon: Icon(
+                  Icons.group_add,
+                  color: Styles.drg_colorDarkerGreen,
+                ),
+                tooltip: "Add new Survey",
+                onPressed: () {
+                  surveyBloc.currentAskedPerson = 'Anonymous';
+                  surveySetBloc.setCurrentPrismSurveySetId(id: arg['id']);
+                  Navigator.pushNamed(context, '/draggerscaffold');
+                },
+              ),
+              bottomNavigationBar: !moreSurveyThanOne ? null : BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                key: ValueKey(arg.hashCode),
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    title: Text('Details'),
+                    icon: Icon(Icons.dvr),
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.show_chart),
+                    title: Text('Graphs'),
+                  ),
+                ],
+                currentIndex: _selectedIndex,
+                backgroundColor: Styles.drg_colorSecondary,
+                unselectedItemColor: Styles.drg_colorAppBackground,
+                selectedItemColor: Styles.drg_colorContrast,
+                onTap: (int index) => setState(() => _selectedIndex = index),
+              ));
+        });
   }
 
   List<Widget> _widgetOptions(context, {args}) {
