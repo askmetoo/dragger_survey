6391d092f5b8f611b90a7d5b1543a7f98c12945f
dthuesen <dthuesen@gmx.de>
2019-12-02 15:30:34 +0100

    Rest of merge

--- lib/src/blocs/team_bloc.dart
+++ lib/src/blocs/team_bloc.dart
@@ -69,15 +69,6 @@ class TeamBloc extends ChangeNotifier {
     notifyListeners();
   }
 
-  setCurrentSelectedTeamId(String selectedTeamId) {
-    _currentSelectedTeamId = selectedTeamId;
-    //// This call for notifyListeners produces an assertion exeption:
-    ///    setState() or markNeedsBuild() called during build.
-    ///  But leaving it out won't update the SurveySets list after
-    ///  selecting a team from the teams dropdown.
-    notifyListeners();
-  }
-
   Stream<QuerySnapshot> get streamTeams {
     return Collection<Team>(path: 'teams').streamDocuments();
   }
